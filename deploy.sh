fd1c55a5b9ec8d3096259b9572baf15bd7f44e20
etherio <git@myan.app>
2020-12-17 05:05:18 +0630

    Bug Fixed and Changes • Updated deploy.sh • Add new file:  docs/CNAME • Add new file:  docs/css/app.f498755e.css • Add new file:  docs/css/chunk-vendors.9bd88064.css • Add new file:  docs/favicon.ico • Add new file:  docs/index.html • Add new file:  docs/js/app.cfba2085.js • Add new file:  docs/js/app.cfba2085.js.map • Add new file:  docs/js/chunk-vendors.e11b02ae.js • Add new file:  docs/js/chunk-vendors.e11b02ae.js.map • Updated package.json

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-git subtree push --prefix $1 origin gh-pages
+DOCS="$(pwd)/docs"
+DIST="$(pwd)/dist"
+CNAME="${DOCS}/CNAME"
+
+cp "${CNAME}" "${DIST}/$(basename "${CNAME}")"
+rm -r "${DOCS}"
+mv "${DIST}" "$(basename "${DOCS}")"
