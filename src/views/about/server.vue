08b23f96397012bd8e6190584dd30762d1c51ffc
Etherio <50423290+etherio97@users.noreply.github.com>
2021-01-25 21:16:35 +0630

    Update server.vue

--- src/views/about/server.vue
+++ src/views/about/server.vue
@@ -23,14 +23,27 @@
 	</v-icon>
       </v-card-title>
       <v-card-subtitle>
-        {{ requested }} requests
+        <v-simple-table>
+          <tr>
+            <th>Total Counts</th>
+            <td>{{ requested || '-' }} times</td>
+          </tr>
+          <tr>
+            <th>Server Uptime</th>
+            <td>{{ uptime || '-' }} seconds</td>
+          </tr>
+          <tr>
+            <th>Requested IP</th>
+            <td>{{ ipAddr }}</td>
+          </tr>
+        </v-simple-table>
       </v-card-subtitle>
     </v-card>
     <v-divider class="mt-2 mb-2" />
     <!-- Memory -->
     <v-card>
       <v-card-title>
-	RAM
+	Memory (RAM)
 	<v-spacer />
         <v-icon class="pr-2">
 	  mdi-memory
@@ -39,12 +52,20 @@
       <v-card-text>
         <v-simple-table>
 	  <tr>
-	    <th>Allocated</th>
-	    <td>{{ allocated }}</td>
+	    <th>Resident Set Size (RSS)</th>
+	    <td>{{ rss }} bytes</td>
 	  </tr>
 	  <tr>
-	    <th>Used</th>
-	    <td>{{ used }}</td>
+	    <th>Heap Total</th>
+	    <td>{{ heapTotal }} bytes</td>
+	  </tr>
+	  <tr>
+	    <th>Heap Used</th>
+	    <td>{{ heapUsed }} bytes</td>
+	  </tr>
+	  <tr>
+	    <th>External</th>
+	    <td>{{ external }} bytes</td>
 	  </tr>
         </v-simple-table>
       </v-card-text>
@@ -55,32 +76,66 @@
 <script>
 import server from "@/app/server";
 
+let eid;
+let stt;
+
 export default {
   data: () => ({
     loading: true,
     active: false,
     pending: false,
     failed: false,
-    requested: null,
-    allocated: null,
-    used: null,
+    ipAddr: null,
+    processor: null,
+    rss: null,
+    count: null,
+    heapTotal: null,
+    heapUsed: null,
+    uptime: null,
   }),
+
+  methods: {
+    uptimeInterval() {
+      try {
+        eid = setInterval(() => {
+          this.uptime = Math.round(Date.now() - stt, 2);
+        }, 800);
+      } catch(e) {
+        console.error(e);
+        clearInterval(eid);
+        this.uptime = eid = undefined;
+      }
+    }
+  },
+
+  beforeDestroy() {
+    if (eid) {
+      clearInterval(eid);
+      eid = undefined;
+    }
+  },
 
   created() {
     this.pending = true,
     this.axios
       .get(server.status)
-      .then(({data}) => {
+      .then(({ data }) => {
         this.active = true;
-        this.pending = this.failed = false;
-	this.loading = false;
-	this.requested = data.requested;
-	this.allocated = data.memory.allocated;
-	this.used = data.memory.usage;
+        this.pending = this.failed = this.loading = false;
+	this.ipAddr = data.ip;
+        this.count = data.count;
+        this.processor = null;
+        this.rss = data.memory.rss;
+        this.external = data.memory.external;
+	this.heapTotal = data.memory.heapTotal;
+	this.heapUsed = data.memory.heapUsed;
+        this.uptime = data.uptime;
+        stt = Date.now() - (data.uptime * 1000);
+        this.uptimeInterval();
       })
       .catch((err) => {
-	alert('ERROR:' + err.message);
-	this.loading = false;
+	this.failed = true;
+	this.pending = this.active = this.loading = false;
       });
   }
 }
