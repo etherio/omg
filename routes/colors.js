88c48542818a641da3a90be744117671bdc864f6
Shin Maung <ethereal97@gmail.com>
2021-01-25 12:37:51 +0630

    updated data color & code

--- routes/colors.js
+++ routes/colors.js
@@ -25,17 +25,34 @@ router.get('/', guard.firebase('admin', 'moderator'), async (req, res) => {
 router.post("/", guard.firebase("admin"), async (req, res) => {
   try {
     const title = (req.body.title || '').trim().toLowerCase();
+    const code = req.body.code || undefined;
     if (!title) throw { code: 400, message: 'title required' };
     const id = crc32(title);
     const db = database().ref(`${databaseName}/colors`).child(id);
     const ref = await db.get();
     if (ref.exists()) throw { code: 200, message: 'already existed' };
-    await db.set({ title, createdAt: database.ServerValue.TIMESTAMP });
-    res.json({ id, title });
+    await db.set({ title, code, createdAt: database.ServerValue.TIMESTAMP });
+    res.json({ id, title, code });
   } catch(e) {
     res.status(e.code || 502).json({ status: e.code || 502, message: e.message });
   }
 });
+
+router.post("/:id", guard.firebase("admin"), async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { code } = req.body;
+    if (!code) throw { code: 400, message: 'required color code' };
+    const db = database().ref(`${databaseName}/colors`).child(id);
+    const ref = await db.get();
+    if (!ref.exists()) throw { code: 404, message: 'not found' };
+    await db.update({ code, updatedAt: database.ServerValue.TIMESTAMP });
+    res.json({ id, code });
+  } catch(e) {
+    res.status(e.code || 502).json({ status: e.code || 502, message: e.message });
+  }
+});
+
 
 router.delete('/:id', guard.firebase('admin', 'moderator'), async (req, res) => {
   try {
