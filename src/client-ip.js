9690c44465027883fc9e8135adf9f6543bbca988
Shin Maung <ethereal97@gmail.com>
2021-01-26 19:26:35 +0630

    added client ip info

--- src/client-ip.js
+++ src/client-ip.js
@@ -1,16 +1,8 @@
 const IPCONFIG_URL = 'https://ipinfo.io';
 const axios = require('axios').default;
 
-async function getIpInfo(ipAddr) {
-  return (await axios(`${IPCONFIG_URL}/${ipAddr}`)).data;
-}
-
 module.exports = function clientIp(ipAddr) {
-  return new Promise(async (resolve) => {
-    try {
-      resolve(await getIpInfo(ipAddr));
-    } catch (e) {
-      resolve({})
-    }
-  });
+  return axios(`${IPCONFIG_URL}/${ipAddr}`)
+    .then(({ data }) => data)
+    .catch((err) => null);
 };
